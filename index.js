9b48817e77ff7b772a73ccb212471c93bbefd33a
zelazunio <ja_zel@wp.pl>
2022-09-11 16:17:41 +0200

    Final release

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ app.use(bodyParser.json());
 const routes = require('./routes/routes.js')
 app.use('/', routes);
 
-/* app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-}); */
-
 dbo.connectToServer(function(err) {
     if (err) {
         console.log(err);
